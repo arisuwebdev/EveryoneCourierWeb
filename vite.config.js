3a1b3c3ff87c91e179afa54e1138f05ec2fc8bfa
arisu4 <webdevarisu4@gmail.com>
2026-07-07 17:43:43 +0530

    check

--- vite.config.js
+++ vite.config.js
@@ -41,6 +41,7 @@ import path from "path";
 
 export default defineConfig({
   plugins: [react()],
+  base: "/EveryoneCourierWeb/", // Your GitHub repository name
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
